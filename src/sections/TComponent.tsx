1beaf0878824fe8499919d09f3b73a76b9ae435c
Lin Htut Kyaw <linhtutkyaw.code@gmail.com>
2022-12-25 13:31:29 +0630

    0.0.1 alpha test-0.0.2 added tailwind react vercel @material-tailwind/react

---
+++ src/sections/TComponent.tsx
@@ -0,0 +1,4 @@
+import { Button } from '@material-tailwind/react';
+export default function TComponent() {
+  return <Button>kkkkkk</Button>;
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+const withMT = require('@material-tailwind/react/utils/withMT');
+module.exports = withMT({
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
